9d5a935baa618b35f9caa266d8808e3b7ec6c37d
mehad605 <mehad605@gmail.com>
2026-05-27 18:15:57 +0600

    fix: rename Application class to Warden to fix startup crash

--- app/src/main/java/com/warden/app/Warden.kt
+++ app/src/main/java/com/warden/app/Warden.kt
@@ -3,7 +3,7 @@ package com.warden.app
 import android.app.Application
 import com.google.android.material.color.DynamicColors
 
-class BlockWords: Application() {
+class Warden: Application() {
   override fun onCreate() {
     DynamicColors.applyToActivitiesIfAvailable(this)
     Thread.setDefaultUncaughtExceptionHandler(CrashLogger(this))
